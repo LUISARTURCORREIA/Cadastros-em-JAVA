2700820125a200f04718cc9f76ace8de8a599384
LUISARTURCORREIA <72110275+LUISARTURCORREIA@users.noreply.github.com>
2020-09-29 20:19:14 -0300

    Cadastros em JAVA
    Cadastros em JAVA

---
+++ cadastros/src/cadastros/Cadastros.java
@@ -0,0 +1,12 @@
+package cadastros;
+
+import java.util.Scanner;
+
+public class Cadastros {
+
+    public static void main(String[] args) {
+      Scanner leia = new Scanner(System.in);
+      System.out.println("Informe o Usuário");
+    }
+    
+}
